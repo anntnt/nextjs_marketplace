77a3782edaae77b7a34663926abab83fc57d79b3
An Tran <anpjure@gmail.com>
2024-10-21 21:44:01 +0200

    Correct CI bugs

--- databaseExampleQueries.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE tropical_snacks;
-CREATE USER tropical_snacks WITH ENCRYPTED PASSWORD 'tropical_snacks';
-GRANT ALL PRIVILEGES ON DATABASE tropical_snacks TO tropical_snacks;
-\connect tropical_snacks
-CREATE SCHEMA tropical_snacks AUTHORIZATION tropical_snacks;
