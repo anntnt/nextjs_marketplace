9d3dd35cba61039fe62a2a56b7e22c1a2533b42b
An Tran <anpjure@gmail.com>
2024-10-23 10:41:29 +0200

    Change quantity input from Select to Input

--- app/products/[productId]/productForm.tsx
+++ app/products/[productId]/productForm.tsx
@@ -8,33 +8,29 @@ type Props = {
   productId: number;
 };
 export default function ProductForm(props: Props) {
-  const [quantity, setQuantity] = useState('1');
+  const [quantity, setQuantity] = useState(1);
+
   async function clickHandler(productId: number, amount: number) {
     await createOrUpdateCartCookie(productId, amount);
   }
   return (
     <div>
       <h3>Amount: {quantity}</h3>
-      <form className={styles.singleProductForm}>
-        <select
-          value={quantity}
-          data-test-id="product-quantity"
-          onChange={(event) => setQuantity(event.currentTarget.value)}
-        >
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
 
+      <form className={styles.singleProductForm}>
         <button
-          formAction={() => clickHandler(props.productId, parseInt(quantity))}
+          type="button"
+          onClick={() => (quantity > 1 ? setQuantity(quantity - 1) : quantity)}
+        >
+          -
+        </button>
+        <input value={quantity} disabled data-test-id="product-quantity" />
+
+        <button type="button" onClick={() => setQuantity(quantity + 1)}>
+          +
+        </button>
+        <button
+          formAction={() => clickHandler(props.productId, quantity)}
           data-test-id="product-add-to-cart"
         >
           Add to cart
