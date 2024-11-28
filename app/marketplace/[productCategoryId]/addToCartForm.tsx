75891b30cd299ac46eacdda597c9f071365578e6
An Tran <anpjure@gmail.com>
2024-11-28 15:25:09 +0100

    Change Cart icon on Button Add to cart

--- app/marketplace/[productCategoryId]/addToCartForm.tsx
+++ app/marketplace/[productCategoryId]/addToCartForm.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { BsCart4 } from 'react-icons/bs';
 import type { Product } from '../../../database/products';
 import type { CreateCartProductResponseBodyPost } from '../../api/cart-items/route';
 import ErrorMessage from '../../ErrorMessage';
@@ -49,23 +50,7 @@ export default function AddToCartForm(props: Props) {
           }}
         >
           <button className="inline-flex items-center rounded-lg bg-blue-1000 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-700 hover:text-white focus:ring-4 focus:ring-blue-300dark:bg-primary-600 ">
-            <svg
-              className="-ms-2 me-2 h-5 w-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6"
-              />
-            </svg>
+            <BsCart4 size={20} className="mr-3 mb-1" />
             Add to cart
           </button>
         </form>
