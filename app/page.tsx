c728aff43b6a86bcbc6fae67778c7854c5fcb666
An Tran <anpjure@gmail.com>
2024-11-29 23:21:03 +0100

    Polish pages

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="bg-gray-50 dark:bg-gray-900 flex-grow  w-full max-w-full px-5 sm:px-20 py-6 sm:py-10 md:py-12">
       <section className=" dark:bg-gray-900">
-        <div className="py-8 px-4 mx-auto max-w-screen-xl  lg:py-16 lg:px-12">
+        <div className="py-8 mt-8 px-4 mx-auto max-w-screen-xl  lg:py-16 lg:px-12">
           <h1 className="text-center mb-4 text-4xl font-semibold tracking-tight leading-none text-gray-900 md:text-4xl lg:text-5xl dark:text-white">
             Welcome to eStores!
           </h1>
@@ -14,13 +14,16 @@ export default function Home() {
             className="py-8
            flex justify-center mb-4  font-semibold tracking-tight leading-none text-gray-900 dark:text-white"
           >
-            <figure className="max-w-2xl">
-              <Link href="/marketplace">
+            <figure className="max-w-2xl rounded-lg border hover:border-black hover:shadow-lg border-gray-200  bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800">
+              <Link
+                href="/marketplace"
+                className="hover:border-black hover:shadow-lg"
+              >
                 <Image
-                  className="h-auto max-w-full rounded-lg"
+                  className="h-auto max-w-full rounded-lg hover:border-black hover:shadow-lg"
                   src="/images/ecommerce.jpg"
                   alt="e-commerce"
-                  width={1279}
+                  width={1023}
                   height={682}
                 />
               </Link>
