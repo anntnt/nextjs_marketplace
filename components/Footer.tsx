5857aa5b6d4d4d2bb4b4e554407f0e9e8b993c30
An Tran <anpjure@gmail.com>
2024-12-05 19:02:14 +0100

    Add content for Page About

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,6 @@ export default function Component() {
       <div className="w-full max-w-full flex flex-col md:flex-row justify-between items-center py-4 px-4 sm:px-10 xl:px-6 2xl:px-20 ">
         <Footer.Copyright href="/" by="eStores™" year={2024} />
         <Footer.LinkGroup>
-          <Footer.Link href="/about">About</Footer.Link>
           <Footer.Link href="#">Privacy Policy</Footer.Link>
           <Footer.Link href="#">Licensing</Footer.Link>
           <Footer.Link href="/contact">Contact</Footer.Link>
