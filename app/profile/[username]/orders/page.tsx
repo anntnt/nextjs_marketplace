19704f398b029e8e92cb378084639ff72ac15412
An Tran <anpjure@gmail.com>
2024-12-05 21:07:19 +0100

    Edit headline of Page My Orders

--- app/profile/[username]/orders/page.tsx
+++ app/profile/[username]/orders/page.tsx
@@ -24,7 +24,7 @@ export default async function UserProfilePage() {
 
   return (
     <main className="flex-grow  w-full max-w-full px-20 py-12">
-      <h1 className="mb-4 text-4xl text-center">Messages Inbox</h1>
+      <h1 className="mb-4 text-4xl text-center">My Orders</h1>
       <section className="py-8 antialiased  md:py-16     text-xl  font-normal   sm:px-16 xl:px-48 dark:text-gray-400">
         <div className="mx-auto text-center max-w-screen-xl px-4 2xl:px-0">
           <p>
