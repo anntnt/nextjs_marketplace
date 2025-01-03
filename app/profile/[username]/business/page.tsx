0d68e079b104d3e67d23aa4e8ac8f34508d273c8
An Tran <anpjure@gmail.com>
2025-01-03 15:13:03 +0100

    Update product name style on hover in seller's My Products page

--- app/profile/[username]/business/page.tsx
+++ app/profile/[username]/business/page.tsx
@@ -67,7 +67,10 @@ export default async function SellerProductsPage() {
                           data-test-id={`seller-product-id-${product.id}`}
                         >
                           <td className="whitespace-nowrap py-2 md:w-[384px]">
-                            <Link href={`/marketplace/product/${product.id}`}>
+                            <Link
+                              href={`/marketplace/product/${product.id}`}
+                              className="hover:underline"
+                            >
                               <div className="flex items-center gap-4">
                                 <div className="flex items-center aspect-square w-75 h-32 shrink-0">
                                   <Image
