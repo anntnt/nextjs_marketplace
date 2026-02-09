6af7088fdf52e8c9dbf28e01ebee930a652d763c
Ann Tran <anpjure@gmail.com>
2026-02-09 16:55:00 +0100

    chore: reinstall pnpm globally for Node v22

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,1 @@
-import upleveled from 'eslint-config-upleveled';
-
-const config = upleveled.map((entry) => {
-  if (!entry.rules) {
-    return entry;
-  }
-
-  return {
-    ...entry,
-    rules: {
-      ...entry.rules,
-      '@ts-safeql/check-sql': 'off',
-    },
-  };
-});
-
-export default config;
+export { default } from 'eslint-config-upleveled';
