1d84bf30b580a3b9954683ad81132e625b964750
anntnt-vienna <antnt1976@gmail.com>
2026-05-31 12:23:45 +0200

    Hide empty error message state

--- components/ui/ErrorMessage.tsx
+++ components/ui/ErrorMessage.tsx
@@ -5,5 +5,8 @@ type Props = {
 };
 
 export default function ErrorMessage(props: Props) {
+  if (!props.children) {
+    return null;
+  }
   return <div className="rounded-md border border-red-500 bg-red-50 px-3 py-2 text-sm text-red-700">{props.children}</div>;
 }
